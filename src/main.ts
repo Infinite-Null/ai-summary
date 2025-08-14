7ef58efb72741795e6da82cd5c31cfcc4bcb428e
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-08-14 11:13:32 +0530

    chore: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('AI Summary API')
+		.setDescription('The AI Summary API description')
+		.setVersion('1.0')
+		.build();
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+	/**
+	 * Setup Swagger UI for the application.
+	 * Visible at `/api` endpoint.
+	 *
+	 * @see https://docs.nestjs.com/openapi/introduction#bootstrap
+	 */
+	SwaggerModule.setup('api', app, documentFactory);
+
+	/**
+	 * Enable versioning for the application.
+	 *
+	 * @see https://docs.nestjs.com/techniques/versioning#uri-versioning-type
+	 */
 	app.enableVersioning({ type: VersioningType.URI });
 
 	await app.listen(process.env.PORT ?? 3000);
