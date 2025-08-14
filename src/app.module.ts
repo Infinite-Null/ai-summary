cb15cd565649f64544749fa1e8d5d68466a41643
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-08-14 11:02:32 +0530

    chore: remove boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AiEngineModule } from './ai-engine/ai-engine.module';
 
 @Module({
 	imports: [AiEngineModule],
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
