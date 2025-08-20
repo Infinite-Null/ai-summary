35435a6ebf3b88bbb2b3488dc87424bda81e96e6
Sainath Poojary <spoojary614@gmail.com>
2025-08-20 18:00:58 +0530

    refactor: Update SlackTestController text and description

--- src/slack/slack-test.controller.ts
+++ src/slack/slack-test.controller.ts
@@ -3,16 +3,16 @@ import { SlackService } from './slack.service';
 import type { FetchMessagesParams } from './types';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@ApiTags('Slack - Testing')
-@Controller('slack-test')
+@ApiTags('Slack')
+@Controller('slack')
 export class SlackTestController {
 	constructor(private readonly slackService: SlackService) {}
 
 	@Post('messages')
 	@ApiOperation({
-		summary: 'Test endpoint - Fetch messages from Slack',
+		summary: 'Fetch messages from Slack',
 		description:
-			'This is a temporary endpoint for testing the Slack integration',
+			'This endpoint retrieves messages from a specific Slack channel.',
 	})
 	async getMessages(@Body() params: FetchMessagesParams) {
 		return this.slackService.getStandups(params);
